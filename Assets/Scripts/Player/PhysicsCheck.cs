e416c8358612f79c9a9e52f5ae7098557b5d13a1
yaoguai <18183849010@163.com>
2024-11-03 21:01:40 +0800

    新增组件和输入控制角色移动

---
+++ Assets/Scripts/Player/PhysicsCheck.cs
@@ -0,0 +1,24 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+public class PhysicsCheck : MonoBehaviour
+{
+    [Header("检测参数")]
+    public float checkRadius;
+    public Vector2 bottomDffset;
+    public LayerMask groundLayer;
+    [Header("状态")]
+    public bool isGround;
+
+
+    private void Update()
+    {
+        //检测地面
+        isGround = Physics2D.OverlapCircle(transform.position, checkRadius, groundLayer);
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.DrawWireSphere((Vector2)transform.position + bottomDffset, checkRadius);
+    }
+}
---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.WSA;
+
+public class PlayerController : MonoBehaviour
+{
+    public InputSystem_Actions inputControl;
+    private Rigidbody2D rb;
+    private SpriteRenderer spriteRenderer;
+    private PhysicsCheck physicsCheck;
+    [Header("运动参数")]
+    [SerializeField] Vector2 inputDirection;
+    [SerializeField] float moveSpeed;
+    [SerializeField] float jumpForce;
+
+    private void Awake()
+    {
+        inputControl = new InputSystem_Actions();
+        inputControl.GamePlay.Jump.started += PlayerJump;
+        rb = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        physicsCheck = GetComponent<PhysicsCheck>();
+    }
+
+    private void Start()
+    {
+
+    }
+
+    private void OnEnable()
+    {
+        inputControl.Enable();
+    }
+
+    private void OnDisable()
+    {
+        inputControl.Disable();
+    }
+
+    private void Update()
+    {
+        inputDirection = inputControl.GamePlay.Move.ReadValue<Vector2>();
+    }
+
+    private void FixedUpdate()
+    {
+        PlayerMove();
+    }
+
+    private void PlayerMove()
+    {
+        rb.linearVelocityX = inputDirection.x * moveSpeed * Time.deltaTime;
+        //角色朝向
+        if (inputDirection.x > 0)
+            spriteRenderer.flipX = false;
+        else if (inputDirection.x < 0)
+            spriteRenderer.flipX = true;
+    }
+
+    private void PlayerJump(InputAction.CallbackContext context)
+    {
+        //Debug.Log("跳跃按钮");
+        if (physicsCheck.isGround)
+            rb.AddForce(transform.up * jumpForce, ForceMode2D.Impulse);
+    }
+
+}
