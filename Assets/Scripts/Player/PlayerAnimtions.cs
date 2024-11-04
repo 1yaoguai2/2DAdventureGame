1395095a89029762fac510efd7d415a7385df669
yaoguai <18183849010@163.com>
2024-11-04 22:22:46 +0800

    新增角色动画

---
+++ Assets/Scripts/Player/PlayerAnimtions.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class PlayerAnimtions : MonoBehaviour
+{
+    private Animator animator;
+    private Rigidbody2D rb;
+    private PhysicsCheck physicsCheck;
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
+        physicsCheck = GetComponent<PhysicsCheck>();
+    }
+
+    private void FixedUpdate()
+    {
+        SetAnimations();
+    }
+
+    void SetAnimations()
+    {
+        animator.SetFloat("speed",Mathf.Abs(rb.linearVelocityX));
+        animator.SetFloat("jumpSpeed",Mathf.Abs(rb.linearVelocityY));
+        animator.SetBool("isGround",physicsCheck.isGround);
+    }
+}
